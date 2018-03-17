0603dc8d1ab4e289a3f84e8439088bb4e1820f98
Emilio Cobos Álvarez <emilio@crisal.io>
2018-03-17 17:58:32 +0100

    try to add a simple test.

---
+++ tests/mailmap/mailmap.c
@@ -0,0 +1,35 @@
+#include "clar.h"
+#include "clar_libgit2.h"
+
+#include "common.h"
+#include "git2/mailmap.h"
+
+static git_mailmap *mailmap = NULL;
+
+const char TEST_MAILMAP[] =
+	"Foo bar <foo@bar.com> <foo@baz.com>\n"
+	"Blatantly invalid line\n";
+
+void test_mailmap_basic__initialize(void)
+{
+	cl_git_pass(git_mailmap_parse(&mailmap, TEST_MAILMAP, sizeof(TEST_MAILMAP)));
+}
+
+void test_mailmap_basic__cleanup(void)
+{
+	if (mailmap) {
+		git_mailmap_free(mailmap);
+		mailmap = NULL;
+	}
+}
+
+void test_mailmap_basic__entry_count(void)
+{
+	git_mailmap_entry* entry;
+
+	cl_assert(git_mailmap_entry_count(mailmap));
+	entry = git_mailmap_entry_byindex(mailmap, 0);
+	cl_assert(entry);
+	cl_assert(!entry->replace_name);
+	cl_assert(!git__strcmp(entry->replace_email, "foo@baz.com"));
+}
